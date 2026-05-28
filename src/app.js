d90085dce856d99fae8948260ceeb3082ab6b517
Ephraim Oshawo <zackgaminguniverse@gmail.com>
2026-05-28 13:28:13 +0100

    basic route setup

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
 const app = express()
+const jobs = require("./routes/jobs")
 
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
+app.use("/api/jobs", jobs)
 
 
 module.exports = app
