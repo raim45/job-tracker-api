46c821ac6fd081ffefabe532a1fc256a9678b99d
Ephraim Oshawo <zackgaminguniverse@gmail.com>
2026-05-28 12:00:57 +0100

    separation from server

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
-const app = express()
+const app = require("./src/app")
 
-app.use(cors())
-app.use(helmet())
-app.use(express.json())
 
 app.listen(3000, () => {
     console.log("server running on port 3000")
---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+
+
+module.exports = app
