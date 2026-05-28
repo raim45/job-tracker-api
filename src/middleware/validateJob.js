e618165047b2f82c97331590d657226ae9f7e5a8
Ephraim Oshawo <zackgaminguniverse@gmail.com>
2026-05-28 17:01:39 +0100

    status validation

--- src/middleware/validateJob.js
+++ src/middleware/validateJob.js
@@ -1,7 +1,12 @@
 const validate = (req, res, next) => {
     const job = req.body
-    if(!job.companyName || !job.jobTitle || !job.status || job.dateApplied){
+    if(!job.companyName || !job.jobTitle || !job.status || !job.dateApplied){
         return res.status(400).json({success: false, msg: "incomplete data"})
+    }
+
+    const validStatuses = ["applied", "interview", "rejected", "offer"]
+    if (!validStatuses.includes(job.status)) {
+        return res.status(400).json({success: false, msg: "invalid status"})
     }
     next()
 }
