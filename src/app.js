4012bd63c6b2f6666442822d7ec09cffb1bad609
Ephraim Oshawo <zackgaminguniverse@gmail.com>
2026-05-28 17:06:45 +0100

    error handling

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const cors = require("cors")
 const helmet = require("helmet")
 const app = express()
 const jobs = require("./routes/jobs")
+const errors = require("./middleware/errorHandler")
 
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
 app.use("/api/jobs", jobs)
+app.use(errors)
 
 
 module.exports = app
